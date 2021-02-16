513812d936270338fc364050296da84cc07145c3
Lays Rodrigues <laysrodriguessilva@gmail.com>
2021-02-16 15:40:08 -0300

    Resolve 06_not_bad
    Signed-off-by: Lays Rodrigues <laysrodriguessilva@gmail.com>

--- 06_not_bad.py
+++ 06_not_bad.py
@@ -8,11 +8,33 @@ por 'good' e retorne a string resultante.
 
 Exemplo: 'The dinner is not that bad!' retorna 'The dinner is good!'
 """
+import re
+
+def find_index(s, word):
+    lower = s.lower()
+    find  = lower.find(word)
+    rfind = lower.rfind(word)
+    print(word, find, rfind)
+    if find == rfind: 
+        return find
+    if find != -1 and rfind != -1: 
+        return min(find,rfind)
 
 def not_bad(s):
     # +++ SUA SOLUÇÃO +++
-    return
+    # Usando find e manipulando a string
+    # bad_idx = find_index(s, 'bad')
+    # not_idx = find_index(s, 'not')
+    # if bad_idx > not_idx :
+    #     return s.replace(s[not_idx:bad_idx+3], 'good')
+    # return s
 
+    # Usango regex
+    # reg = re.compile('(not.+bad)', re.IGNORECASE)
+    # match = reg.search(s)
+    # if match:
+    #     return s.replace(s[match.start():match.end()], 'good')
+    # return s
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
 
@@ -39,3 +61,12 @@ if __name__ == '__main__':
     test(not_bad, 'This dinner is not that bad!', 'This dinner is good!')
     test(not_bad, 'This tea is not hot', 'This tea is not hot')
     test(not_bad, "It's bad yet not", "It's bad yet not")
+    test(not_bad, 'Not not bad', 'good')
+    # Esse falha no regex, pois teria que pegar o grupo (Not bad) 
+    # em vez disso da match entre o primeiro not e o ultimo bad
+    test(not_bad, 'Not bad not bad', 'good not bad')
+    # Esse falha na lógica comum, pois como trabalhamos com a 
+    # primeira aparição da dupla not bad, a lógica segue com o menor
+    # index de cada uma. Imagino que precisaria de tratar esse 
+    # caso especial
+    test(not_bad, 'bad not bad', 'bad good')
